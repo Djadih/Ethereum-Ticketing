eb0fd60c2d94ac71713d3812d74b4f29757e351d
Hussam Djadi <djadih@utexas.edu>
2021-04-02 17:35:34 -0500

    continuing progress

--- contracts/tickets.sol
+++ contracts/tickets.sol
@@ -1,1 +1,38 @@
-pragma solidity >=0.4.22;
+// contracts/MyNFT.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Tickets is ERC721 {
+
+    mapping(uint => address) IdxtoHolder;
+
+
+
+    constructor() ERC721(ticketHolders, ) {
+    }
+
+    struct ticket {
+        address ticketHodler;
+        string eventName;
+        bool sold;
+        bool used;
+    }
+
+    struct Event {
+        address[] ticketHodlers;
+        address payable eventHolder;
+        string eventName;
+        string URL;
+        
+        uint ticketPrice;
+        uint numLeft;
+        uint maxPurchase;
+    }
+
+    function purchaseTicket() public payable {
+
+    }
+
+}
