d712868b5fcf80899cfdda5d26aba86202fc969a
Hussam Djadi <djadih@utexas.edu>
2021-04-05 22:01:38 -0500

    improved tickets.sol. Need to fix createEvent and implement transferTicket as well as destroyEvent.

--- contracts/tickets.sol
+++ contracts/tickets.sol
@@ -19,29 +19,28 @@ contract Tickets is ERC721 {
 
         uint numTicketsPurchased;
 
-        mapping (address => uint) tokensOwnedByAddress;
+        bool[] ticketUsed;
     }
 
     Event[] private events;
+    mapping (address => uint) tokensOwnedByAddress;
 
     // Returns the index of all the events that this owner owns in the "events" array
     mapping (address => uint256[]) private eventOwners;
 
-    function transferTicket(address _to, uint _quantity) public payable {
-
-    }
+    function transferTicket(address _to, uint _quantity) public payable;
 
     function purchaseTicket(uint eventIdx, uint quantity) public payable returns (bool) {
-        Event thisEvent = events[eventIdx];
+        Event storage thisEvent = events[eventIdx];
 
         //require (quantity <= thisEvent.maxPurchaseAmount, "Cannot purchase more than the maxPurchaseAmount for this ticket");
-        //require (thisEvent.numTicketsPurchased < thisEvent.maxSupply, "This event has sold out of tickets");
-        require (msg.value >= thisEvent.ticketPrice.mul(quantity), "Please send enough money");
+        require (thisEvent.numTicketsPurchased < thisEvent.maxSupply, "This event has sold out of tickets");
+        require (msg.value >= thisEvent.ticketPrice, "Please send enough money");
 
         thisEvent.numTicketsPurchased;
         
         //for (uint i = 0; i < quantity ; i++) {
-        thisEvent.tokensOwnedByAddress[msg.sender] = thisEvent.numTicketsPurchased;
+        tokensOwnedByAddress[msg.sender] = thisEvent.numTicketsPurchased;
         _mint(msg.sender, thisEvent.numTicketsPurchased);
         thisEvent.numTicketsPurchased++;
         //}
@@ -49,34 +48,38 @@ contract Tickets is ERC721 {
         return true;
     }
 
-    function useTicket(uint tokenId) public {
-        thisEvent = events[eventIdx];
-        require (thisEvent.tokensOwnedByAddress[msg.sender] == tokenId, "You do not own this Token");
-
+    function useTicket(uint eventIdx, uint tokenId) public returns (bool) {
+        Event storage thisEvent = events[eventIdx];
+        require (tokensOwnedByAddress[msg.sender] == tokenId, "You do not own this Token");
+        require (thisEvent.ticketUsed[tokenId] == false, "This ticket has already been used");
 
         _burn(tokenId);
+
+        thisEvent.ticketUsed[tokenId] = true;
+
+        return true;
     }
 
     function createEvent (string memory Name, string memory URL, uint Price, uint totalTkts, uint maxBuy) public {
-        Event memory ev1 = Event({
-        // ticketHolders : address[totalTkts],
-        eventHolder : msg.sender,
-        eventName : Name,
-        URL : URL,
-        ticketPrice : Price,
-        maxSupply : totalTkts,
-        //maxPurchaseAmount : maxBuy,
-        numTicketsPurchased : 0
-        });
+        Event memory ev1;
+        ev1.eventHolder = msg.sender;
+        // // ticketHolders : address[totalTkts],
+        // eventHolder : msg.sender,
+        // eventName : Name,
+        // URL : URL,
+        // ticketPrice : Price,
+        // maxSupply : totalTkts,
+        // //maxPurchaseAmount : maxBuy,
+        // numTicketsPurchased : 0
+        // });
 
-        uint eventIdx = events.push(ev1);
+        events.push(ev1);
+        uint eventIdx = events.length-1;
+
         eventOwners[msg.sender].push(eventIdx);
-
-        eventToStartingTokenID[eventIdx] = 
-
-        // Mint the maximum supply of tokens all at once
-        mintTokens(eventIdx);
     }
+
+    function destroyEvent() public payable;
 
     // helper functions
     //function getTicketHolders();
